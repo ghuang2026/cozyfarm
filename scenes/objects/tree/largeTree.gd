dbfc7bd9fdd620e9145acf53102b828a146b0fff
ghuang2026 <ghuang2026@gmail.com>
2026-08-26 11:15:05 -0500

    large tree

---
+++ scenes/objects/tree/largeTree.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+@onready var damageComponent: DamageComponent = $damageComponent
+@onready var hurtComponent: HurtComponent = $HurtComponent
+
+var logScene = preload("res://scenes/objects/tree/log.tscn")
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	hurtComponent.hurt.connect(onHurt)
+	damageComponent.maxDamageReached.connect(onMaxDamageReached)
+
+func onHurt(hit: int) -> void:
+	print("hit")
+	damageComponent.applyDamage(hit)
+
+func onMaxDamageReached() -> void:
+	call_deferred("addLogScene")
+	print("no more hp")
+	queue_free()
+
+func addLogScene() -> void:
+	for i in range(3):
+		var logInstance = logScene.instantiate() as Node2D
+		logInstance.global_position = global_position.move_toward(
+			Vector2(global_position.x + rng.randf_range(-10.0, 10.0), 
+					global_position.y + rng.randf_range(-10.0, 10.0) + 20),
+			20)
+		get_parent().add_child(logInstance)
