87aad8d4da364a1cc5b24135008a2b37bfd4f87d
ghuang2026 <ghuang2026@gmail.com>
2026-09-05 21:04:59 -0500

    debug code

--- scripts/stateMachine/nodeStateMachine.gd
+++ scripts/stateMachine/nodeStateMachine.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	if initial_node_state:
 		initial_node_state._on_enter()
 		current_node_state = initial_node_state
+		current_node_state_name = current_node_state.name.to_lower()
 
 
 func _process(delta : float) -> void:
@@ -46,4 +47,4 @@ func transition_to(node_state_name : String) -> void:
 	
 	current_node_state = new_node_state
 	current_node_state_name = current_node_state.name.to_lower()
-	#print("Current State: ", current_node_state_name)
+	print("Current State: ", current_node_state_name)
