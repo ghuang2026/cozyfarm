680d27f289e93c22d61bcfbe378e8cd7bd7f1eff
ghuang2026 <ghuang2026@gmail.com>
2026-09-22 14:52:51 -0500

    fixed button for speeding time up effects

--- scenes/ui/time_panel.gd
+++ scenes/ui/time_panel.gd
@@ -2,10 +2,13 @@ extends Control
 
 @onready var day_label: Label = $DayTimeCounter/MarginContainer/Day
 @onready var time_label: Label = $DayTimeCounter/MarginContainer/Time
+@onready var normal_speed_button: Button = $Control/NormalSpeedButton
+@onready var fast_speed_button: Button = $Control/FastSpeedButton
+@onready var very_fast_speed_button: Button = $Control/VeryFastSpeedButton
 
 @export var normal_speed: int = 10
 @export var fast_speed: int = 100
-@export var very_fast_speed: int = 200
+@export var very_fast_speed: int = 300
 
 func _ready() -> void:
 	DayNightManager.time_tick.connect(on_time_tick)
@@ -26,10 +29,25 @@ func on_time_tick(day:int, hour:int, minute:int) -> void:
 		time_label.text = "%02d:%02d" % [hour, minute]
 
 func _on_normal_speed_button_pressed() -> void:
-	DayNightManager.game_speed = normal_speed
+	if DayNightManager.game_speed == normal_speed:
+		DayNightManager.game_speed = normal_speed
+		normal_speed_button.release_focus()
+	else:
+		DayNightManager.game_speed = normal_speed
+		normal_speed_button.grab_focus()
 
 func _on_fast_speed_button_pressed() -> void:
-	DayNightManager.game_speed = fast_speed
+	if DayNightManager.game_speed == fast_speed:
+		DayNightManager.game_speed = normal_speed
+		fast_speed_button.release_focus()
+	else:
+		DayNightManager.game_speed = fast_speed
+		fast_speed_button.grab_focus()
 
 func _on_very_fast_speed_button_pressed() -> void:
-	DayNightManager.game_speed = very_fast_speed
+	if DayNightManager.game_speed == very_fast_speed:
+		DayNightManager.game_speed = normal_speed
+		very_fast_speed_button.release_focus()
+	else:
+		DayNightManager.game_speed = very_fast_speed
+		very_fast_speed_button.grab_focus()
